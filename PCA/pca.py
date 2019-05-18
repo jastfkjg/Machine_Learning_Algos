b29b49fccd04e58c29c306b9943c22fc13294c09
Zilong <550983776@qq.com>
2019-05-18 12:56:15 +0800

    add pca.py

--- PCA/pca.py
+++ PCA/pca.py
@@ -7,4 +7,24 @@ def loadDataSet(fileName, delim='\t'):
         datArr = [map(float,line) for line in stringArr]
     return np.mat(datArr)
 
+def pca(data, topNfeat):
+    """
+    topNfeat: how many eigen vectors to take, or dim of new space
+    """
+    mean = np.mean(data, axis=0)
+    meanRemoved = data - mean
+    # calculate covariance matrix
+    cov = np.cov(meanRemoved, rowvar=0)
+    # calculate eigen values and eigen vectors
+    eigvals, eigVects = np.linalg.eig(np.mat(cov))
+    # sort according to eigen values
+    eigValInd = np.argsort(eigVals)
+    eigValInd = eigValInd[:-(topNfeat+1):-1]
+    redEigVects = eigVects[:, eigValInd]
+    lowData = meanRemoved * redEigVects
+    recon = (lowData * redEigVects) + mean
+    return lowData, recon 
+
+
+
 
